0a3c53a99f92cd80d0b05896a7a79da9587992e9
garciis <pgarciab9@alumnos.nebrija.es>
2024-05-17 12:03:36 +0200

    retrofit

---
+++ app/src/main/java/com/example/tfg/Retrofit/Response/ReservaResponse.kt
@@ -0,0 +1,9 @@
+package com.example.tfg.Retrofit.Response
+
+import com.example.tfg.Retrofit.DataClases.Reserva
+
+data class ReservaResponse(
+    var codigo: String,
+    var mensaje: String,
+    var data: ArrayList<Reserva>
+)
