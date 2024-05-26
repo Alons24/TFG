b5164c9210f9e5ca008d72c4cd9ceb3cf24a1dd6
Alons24 <alonsocallejagarces@gmail.com>
2024-05-26 21:39:46 +0200

    última actualización día 26/5/2024

--- app/src/main/java/com/example/tfg/Screens/PantallasClientes/DejarResena.kt
+++ app/src/main/java/com/example/tfg/Screens/PantallasClientes/DejarResena.kt
@@ -1,5 +1,6 @@
 package com.example.tfg.Screens.PantallasClientes
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.icons.Icons
@@ -40,19 +40,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.tfg.R
 import com.example.tfg.navigation.AppScreens
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DejarResena(navController: NavHostController) {
-    val scaffoldState = rememberScrollState()
+    val scrollState = rememberScrollState() // Inicializar scrollState
     val scope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-
 
     /*Inicio del cajón lateral*/
     ModalNavigationDrawer(
@@ -68,7 +69,7 @@ fun DejarResena(navController: NavHostController) {
                     //BOTÓN PARA BOLVER AL MENÚ DE INICIO
                     // Otros elementos del menú lateral
                     Button(
-                        onClick = { /*navController.navigate(AppScreens.Despensa.ruta)*/ },
+                        onClick = { /*navController.navigate(AppScreens.DejarResena.ruta) */},
                         modifier = Modifier
                             .fillMaxWidth()
                             .width(300.dp)
@@ -77,14 +78,14 @@ fun DejarResena(navController: NavHostController) {
                         colors = ButtonDefaults.buttonColors(Color(4, 104, 249, 255))
                     ) {
                         Text(
-                            text = "Escaner QR",
+                            text = "Escaner qr",
                             fontSize = 40.sp,
                         )
                     }
 
 
                     Button(
-                        onClick = { navController.navigate(AppScreens.MenuCategorias.ruta) },
+                        onClick = { navController.navigate(AppScreens.Reservas.ruta) },
                         modifier = Modifier
                             .fillMaxWidth()
                             .width(300.dp)
@@ -93,109 +94,11 @@ fun DejarResena(navController: NavHostController) {
                         colors = ButtonDefaults.buttonColors(Color(4, 104, 249, 255))
                     ) {
                         Text(
-                            text = "MENU PLATOS",
+                            text = "Menu platos",
                             fontSize = 40.sp,
                         )
                     }
 
-
-                    Button(
-                        onClick = { navController.navigate(AppScreens.MenuCategorias.ruta) },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .width(300.dp)
-                            .height(100.dp),
-                        shape = RectangleShape,
-                        colors = ButtonDefaults.buttonColors(Color(4, 104, 249, 255))
-                    ) {
-                        Text(
-                            text = "Hacer reservas",
-                            fontSize = 40.sp,
-                        )
-                    }
-
-                    //FIN DE LOS BOTONES DEL MENÚ LATERAL
-                }
-            }
-        },
-    ) {
-
-
-        //Fin del cajón lateral y enpieza el Scaffold
-
-        Scaffold(
-            topBar = {
-                CenterAlignedTopAppBar(
-                    colors = TopAppBarDefaults.mediumTopAppBarColors(
-                        containerColor = Color.Blue, // Cambia el color de fondo
-                        titleContentColor = Color.White, // Cambia el color del título
-                    ),
-                    title = {
-                        Text("DEJAR RESEÑA")
-
-                    },
-                    navigationIcon = {
-                        IconButton(onClick = { navController.navigate("MenuClientes") }) {
-
-                            Icon(
-                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                contentDescription = "Localized description",
-                                tint=Color.White
-                            )
-                        }
-                    },
-                    actions = {
-                        IconButton(onClick = { scope.launch { drawerState.open() } }) {
-                            Icon(
-                                imageVector = Icons.Filled.Menu,
-                                contentDescription = "Menu",
-                                tint=Color.White
-                            )
-                        }
-                    }
-                )
-            },
-            bottomBar = {
-                BottomAppBar(
-                    containerColor = Color.Blue,
-                    contentColor = MaterialTheme.colorScheme.primary,
-                ) {
-
-
-                    // Icono Adicional
-                    BottomNavigationItem(
-                        selected = false,
-                        onClick = {
-                            /* Código para la acción del segundo ícono */
-                        },
-                        modifier = Modifier.weight(1f),
-                        icon = {
-                            Icon(imageVector = Icons.Default.DateRange, contentDescription = "YourIcon", tint = Color.White)
-                        },
-                    )
-                }
-            },
-        ) { innerPadding ->
-            Box(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .verticalScroll(state = scaffoldState)
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding),
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
-                ) {
-
-                    var resena by remember{ mutableStateOf("") }
-                    OutlinedTextField(value = resena,
-                        onValueChange = {resena=it},
-                        singleLine = true,
-                        label={
-                            Text("Introduzca la reseña que desee")
-                        }
-                    )
 
                     Button(
                         onClick = { navController.navigate(AppScreens.DejarResena.ruta) },
@@ -207,11 +110,115 @@ fun DejarResena(navController: NavHostController) {
                         colors = ButtonDefaults.buttonColors(Color(4, 104, 249, 255))
                     ) {
                         Text(
+                            text = "Hacer reserva",
+                            fontSize = 40.sp,
+                        )
+                    }
+
+
+                    //FIN DE LOS BOTONES DEL MENÚ LATERAL
+                }
+            }
+        },
+    ) {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    colors = TopAppBarDefaults.mediumTopAppBarColors(
+                        containerColor = Color.Blue, // Cambia el color de fondo
+                        titleContentColor = Color.White, // Cambia el color del título
+                    ),
+                    title = {
+                        Text("DEJAR RESEÑA")
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { navController.navigate("MenuClientes") }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = "Localized description",
+                                tint = Color.White
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Menu",
+                                tint = Color.White
+                            )
+                        }
+                    }
+                )
+            },
+            bottomBar = {
+                BottomAppBar(
+                    containerColor = Color.Blue,
+                    contentColor = MaterialTheme.colorScheme.primary,
+                ) {
+                    BottomNavigationItem(
+                        selected = false,
+                        onClick = { /* Código para la acción del segundo ícono */ },
+                        modifier = Modifier.weight(1f),
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.DateRange,
+                                contentDescription = "YourIcon",
+                                tint = Color.White
+                            )
+                        },
+                    )
+                }
+            },
+        ) { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                contentAlignment = androidx.compose.ui.Alignment.TopCenter // Cambiar a la parte superior
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally // Centrar horizontalmente
+                ) {
+                    var resena by remember { mutableStateOf("") }
+                    OutlinedTextField(
+                        value = resena,
+                        onValueChange = { resena = it },
+                        singleLine = true,
+                        label = {
+                            Text("Introduzca la reseña que desee")
+                        },
+                        modifier = Modifier.fillMaxWidth(0.8f) // Ajustar el ancho del TextField
+                    )
+
+                    Button(
+                        onClick = { navController.navigate(AppScreens.DejarResena.ruta) },
+                        modifier = Modifier
+                            .fillMaxWidth(0.8f)
+                            .height(50.dp), // Ajustar la altura del botón
+                        shape = RectangleShape,
+                        colors = ButtonDefaults.buttonColors(Color(4, 104, 249, 255))
+                    ) {
+                        Text(
                             text = "ENVIAR RESEÑA",
                             fontSize = 25.sp,
                         )
                     }
 
+                    // Espaciado para que la imagen se mueva hacia abajo
+                    Box(modifier = Modifier.weight(1f))
+
+                    Image(
+                        painter = painterResource(R.drawable.cervecer_a_montemayorjpg),
+                        contentDescription = "TÍTULO DE LA EMPRESA",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                    )
                 }
             }
         }
